6337adc9c32d9e341838a8b5bf2a81a578af7ee1
mozi <2138844072@qq.com>
2025-05-16 17:53:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "./components/Layout";
+import { HomeWork } from "./components/sections/HomeWork";
+
+function App() {
+	return (
+		<Layout title="HomeWork">
+			<HomeWork />
+		</Layout>
+	);
+}
+
+export default App;
